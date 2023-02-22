7c234042bd2bb6b4f8cab18dcb60b10ab711629f
jainhiya12 <121207496+jainhiya12@users.noreply.github.com>
2023-02-22 10:41:59 -0500

    Create hanoi_tower.py

---
+++ hanoi_tower.py
@@ -0,0 +1,11 @@
+def tower_of_hanoi(n, source, auxiliary, target):
+  if n == 1:
+    print("move disc top from", source, "to", target)
+    return
+  else:
+    tower_of_hanoi(n-1, source, target, auxiliary)
+    print("move disc", n, "from", source, "to", target)
+    tower_of_hanoi(n-1, auxiliary, source, target)
+
+n = 3
+tower_of_hanoi(n, 'A', 'B', 'C')
