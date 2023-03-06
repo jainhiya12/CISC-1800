882a73503024d14c1cfef7d45e1a1216ebd1cd03
jainhiya12 <121207496+jainhiya12@users.noreply.github.com>
2023-03-05 21:13:06 -0500

    Create hw3q2.py

---
+++ hw3q2.py
@@ -0,0 +1,20 @@
+# taking input
+user_input = input("Enter a list of integers separated by commas: ")
+# sepearying input using split
+num_list = user_input.split(",")
+#initialising list to 0
+total = 0
+
+# looping through the list
+for num in num_list:
+# picking out error messages
+    while not num.isdigit():
+        print("Invalid input")
+        user_input = input("Enter a list of integers separated by commas: ")
+        num_list = user_input.split(",")
+        num = num_list[0]
+#summing the numbers
+    total += int(num)
+
+# printing the output
+print("The sum of all the integers in the list is:", total)
